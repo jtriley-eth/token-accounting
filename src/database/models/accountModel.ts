57c5322f1495ab23754c5d23eb00b413690608a0
Joshua Trujillo <Jtriley15@gmail.com>
2021-08-26 14:58:41 -0400

    added accountModel

---
+++ src/database/models/accountModel.ts
@@ -0,0 +1,45 @@
+import { model, Schema } from 'mongoose'
+
+const accountSchema = new Schema({
+	address: String,
+	transfers: [
+		{
+			date: Number,
+			sender: String,
+			recipient: String,
+			txHash: String,
+			networkId: String,
+			amountToken: String,
+			amountFiat: String,
+			exchangeRate: String,
+			token: {
+				id: String,
+				symbol: String,
+				name: String
+			}
+		}
+	],
+	flows: [
+		{
+			date: Number,
+			start: Number,
+			end: Number,
+			sender: String,
+			recipient: String,
+			networkId: String,
+			txHash: String,
+			amountToken: String,
+			amountFiat: String,
+			exchangeRate: String,
+			token: {
+				id: String,
+				symbol: String,
+				name: String
+			}
+		}
+	]
+})
+
+const accountModel = model('accountCollection', accountSchema)
+
+export default accountModel
